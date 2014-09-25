e277f56c9cf93cb4997a893694f915438dd5731a
brycehenley <brycehenley97@gmail.com>
2014-09-25 07:18:12 -0700

    added time function and a fix for gamepad input

--- scripts/Gameover.lua
+++ scripts/Gameover.lua
@@ -3,15 +3,31 @@ gtext = getObject("Text0")
 setText(gtext , "Game Over") 
 setTextColor(gtext, {255, 0, 0, 255})
 
+sec = 0
+min = 0
+timeseta = 0
+
+function time()
+	sec = sec + 1
+	if sec == 60 then
+		min = min + 1
+		sec = 0
+	end
+end
 
 function onSceneUpdate()
+	time()
 
 	if isKeyPressed("ENTER") then
 		loadLevel("levels/titlescreen.level")
 	end
-
-	if isKeyPressed("JOY1_BUTTON_Y") then
+	if onKeyDown("MOUSE_BUTTON_LEFT") then 
 		loadLevel("levels/titlescreen.level")
+	end
+	if isKeyPressed("JOY1_BUTTON_Y") then
+		if sec % 2 == 0 then
+		loadLevel("levels/titlescreen.level")
+		end
 	end
 
 	if isKeyPressed("ESCAPE") then
